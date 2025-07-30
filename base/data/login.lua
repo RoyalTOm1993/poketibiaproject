56f7b65fc74ae33775c8018f170d8e5c0ead8947
Ewerton Gomes <royal.ewerton123@gmail.com>
2025-07-29 22:43:01 -0300

    Add files via upload

--- base/data/login.lua
+++ base/data/login.lua
@@ -1,5 +1,17 @@
+local creatureevent = CreatureEvent("onLoginRev")
 
-function onLogin(player)
-	player:loginHandler()
+function creatureevent.onLogin(player)
+	player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Welcome Ao PokeMaster!")
 	return true
 end
+
+creatureevent:register()
+
+
+local creatureevent = CreatureEvent("onLogoutRev")
+
+function creatureevent.onLogout(player)
+	return true
+end
+
+creatureevent:register()
